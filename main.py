6e1d830b2d32dba88169b2c11d22a18f0290836f
salih aasim <73870591+salihaasim@users.noreply.github.com>
2020-11-24 13:04:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep, strftime
 from random import randint
-import pandas as pd
+
 
 
 chromedriver_path ='C:\\chrome driver\\chromedriver_win32\\chromedriver.exe' # Change this to your own chromedriver path!
@@ -18,10 +18,11 @@ password.send_keys('password') # Change this to your own Instagram password
 button_login = webdriver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button/div')
 button_login.click()
 sleep(3)
-try:
-    notnow = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
-    notnow.click() # Comment these last 2 lines out, if you don't get a pop up asking about notifications
-except :
-    pass
+
+notnow = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button')
+notnow.click() # Comment these last 2 lines out, if you don't get a pop up asking about notifications
+notfication = webdriver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[3]/button[2]')
+notfication.click()
+
 search = webdriver.find_element_by_xpath('//*[@id="react-root"]/section/nav/div[2]/div/div/div[2]/input')
-search.send_keys('irfan view')
+search.send_keys('')#account you want to search
